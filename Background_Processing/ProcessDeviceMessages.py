a5b766ac32dcfce09c128657c210e921fea81d4d
Dan Hendrix <github@behandy.net>
2020-05-12 15:58:44 -0500

    Write Device output to MariaDB. Refine DB structure

--- Background_Processing/ProcessDeviceMessages.py
+++ Background_Processing/ProcessDeviceMessages.py
@@ -3,6 +3,8 @@
 # TODO Add function to create a default config.ini if the file is missing
 # NOTE: Removing config.ini from repo temporarily
 
+import mysql.connector as mariadb
+from mysql.connector import errorcode
 import paho.mqtt.client as mqtt
 import time
 import logging as log
@@ -12,23 +14,74 @@ import numpy as np
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+SensorType = 'TEMP'
 
-log.basicConfig(filename=config['DEFAULT']['LOG_FILE'], level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+log.basicConfig(
+    filename=config['DEFAULT']['LOG_FILE'],
+    level=log.DEBUG,
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S')
+
+def write_data(record):
+    connection = mariadb.connect(host=config['DB']['DB_HOST'],
+                                 user=config['DB']['DB_USERNAME'],
+                                 password=config['DB']['DB_PASSWORD'])
+
+    sql_insert_query = """INSERT INTO device_log (device_id,device_name,data_type,data_payload,created_at)
+                        VALUES (%s,%s,%s,%s,%s)"""
+
+    print("Writing to DB: " + str(record))
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("USE {}".format(config['DB']['DB']))
+        cursor.execute(sql_insert_query, record)
+    except mariadb.Error as err:
+        print(err)
+    else:
+        print(" OK")
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def process_message(DeviceMessage):
+    # 2020-04-29 22:27:41
+    rawMsg = StringIO(DeviceMessage)
+    tmpMsg = np.genfromtxt(rawMsg, dtype=[
+        ('EventDate', 'S10'),
+        ('EventTime', 'S8'),
+        ('DeviceID', 'S20'),
+        ('DataValue', 'S20')
+    ], delimiter="|")
+    FormattedEventDate = str(tmpMsg['EventDate'], 'utf-8')
+    FormattedEventTime = str(tmpMsg['EventTime'], 'utf-8')
+    CombinedDateTime = FormattedEventDate + ' ' + FormattedEventTime
+    FormattedDeviceID = str(tmpMsg['DeviceID'], 'utf-8')
+    TempData = str(tmpMsg['DataValue'], 'utf-8')
+    FormattedData = TempData.split('=', 1)
+
+    data_tuple = ('1', FormattedDeviceID, FormattedData[0], FormattedData[1], CombinedDateTime)
+    print("Record: " + str(data_tuple))
+    write_data(data_tuple)
 
 def on_message(mqttc, obj, msg):
+    global SensorType
     FormattedPayload = str(msg.payload, 'utf-8')
 
-    if "TEMP=" in FormattedPayload:
-        rawMsg = StringIO(FormattedPayload)
-        tmpMsg = np.genfromtxt(rawMsg, dtype=[('EventDate', 'S10'), ('EventTime', 'S8'), ('DeviceID', 'S20'),('TempValue', 'S20')], delimiter="|")
-        FormattedEventDate = str(tmpMsg['EventDate'], 'utf-8')
-        FormattedEventTime = str(tmpMsg['EventTime'], 'utf-8')
-        FormattedDeviceID = str(tmpMsg['DeviceID'], 'utf-8')
-        FormattedTemperature = str(tmpMsg['TempValue'], 'utf-8')
-        FormattedTemperature = FormattedTemperature[5:]
+    if "TEMP" in SensorType:
+        print("SensorType TEMP: " + SensorType)
+        SensorType = 'HUMIDITY'
+        if "TEMP=" in FormattedPayload:
+            print("FormattedPayload: " + str(FormattedPayload))
+            process_message(FormattedPayload)
+    elif "HUMIDITY" in SensorType:
+        print("SensorType HUMIDITY: " + SensorType)
+        SensorType = "TEMP"
+        if "HUMIDITY=" in FormattedPayload:
+            print("FormattedPayload: " + str(FormattedPayload))
+            process_message(FormattedPayload)
 
-        print(FormattedEventDate + "|" + FormattedEventTime + "|" + FormattedDeviceID + "|" + config['DEFAULT']['SUBSCRIBE_CHANNEL'] + "|" + FormattedTemperature)
-        log.info(FormattedEventDate + "|" + FormattedEventTime + "|" + FormattedDeviceID + "|" + config['DEFAULT']['SUBSCRIBE_CHANNEL'] + "|" + FormattedTemperature)
 
 def on_connect(mqttc, obj, flags, rc):
     if ( 'TRUE' == config['DEFAULT']['DEBUG'] ):
@@ -36,7 +89,7 @@ def on_connect(mqttc, obj, flags, rc):
     log.info("Connection return code: " + str(rc))
 
     # Subscribe request
-    MQTTClient.subscribe("test")
+    MQTTClient.subscribe(config['MQTT']['SUBSCRIBE_CHANNEL'])
 
 def on_publish(mqttc, obj, mid):
     if ( 'TRUE' == config['DEFAULT']['DEBUG'] ):
@@ -73,15 +126,13 @@ MQTTClient.on_subscribe = on_subscribe
 
 # Message broker
 # TODO Investigate configparser with timeout and port
-MQTT_Host = config['DEFAULT']['MQTT_HOST']
+MQTT_Host = config['MQTT']['MQTT_HOST']
 print("MQTT_Host = " + MQTT_Host)
-MQTT_Port = config['DEFAULT']['MQTT_PORT']
+MQTT_Port = config['MQTT']['MQTT_PORT']
 print("MQTT_Port = " + str(MQTT_Port))
-MQTT_Timeout = config['DEFAULT']['MQTT_TIMEOUT']
+MQTT_Timeout = config['MQTT']['MQTT_TIMEOUT']
 print("MQTT_Timeout = " + str(MQTT_Timeout))
-MQTTClient.connect(config['DEFAULT']['MQTT_HOST'], 1883, 60)
+MQTTClient.connect(config['MQTT']['MQTT_HOST'], 1883, 60)
 
 # Just what it says...forever
 MQTTClient.loop_forever()
-
-
--- DB_Setup/Setup_DB_Structure.py
+++ DB_Setup/Setup_DB_Structure.py
@@ -2,72 +2,73 @@
 
 from __future__ import print_function
 
-import mysql.connector
+import mysql.connector as mariadb
 from mysql.connector import errorcode
 
 DB_NAME='companies'
-USER_NAME='<insert username here>'
-PASSWORD='<insert DB password here>'
+DB_HOST='<Insert DB hostname or IP here>'
+USER_NAME='<DB Username>'
+PASSWORD='<DB Password>'
 
 TABLES = {}
 TABLES['organization'] = (
     "CREATE TABLE `organization` ("
     "  `organization_id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `organization_name` varchar(50) NULL COMMENT 'Org Name',"
-    "  `location` varchar(50) NULL COMMENT 'Geographic Location',"
-    "  `active` BOOLEAN NOT NULL DEFAULT '1' COMMENT 'Active?',"
-    "  `created_at` date NOT NULL COMMENT 'Date created',"
-    "  `updated_at` date NOT NULL COMMENT 'Date last updated',"
+    "  `location` varchar(250) NULL COMMENT 'Location',"
+    "  `active` tinyint NOT NULL DEFAULT '1' COMMENT 'Active?',"
+    "  `created_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Date created',"
+    "  `updated_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Date last updated' ON UPDATE CURRENT_TIMESTAMP(),"
     "  PRIMARY KEY (`organization_id`)"
     ") ENGINE=InnoDB")
 
-TABLES['buildings'] = (
+TABLES['building'] = (
     "CREATE TABLE `buildings` ("
     "  `building_id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `building_name` varchar(50) NULL COMMENT 'Building Reference',"
+    "  `building_location` varchar(250) NULL COMMENT 'Building location',"
     "  `organization_id` int(11) NOT NULL,"
-    "  `building_type` varchar(50) NOT NULL COMMENT 'Building Purpose',"
-    "  `active` BOOLEAN NOT NULL DEFAULT '1' COMMENT 'Active?',"
-    "  `created_at` date NOT NULL COMMENT 'Date created',"
-    "  `updated_at` date NOT NULL COMMENT 'Date last updated',"
+    "  `building_type` varchar(250) NOT NULL COMMENT 'Building Purpose',"
+    "  `active` tinyint NOT NULL DEFAULT '1' COMMENT 'Active?',"
+    "  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'Date created',"
+    "  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() COMMENT 'Date last updated',"
     "  PRIMARY KEY (`building_id`), UNIQUE KEY `building_name` (`building_name`),"
-    "  CONSTRAINT `buildings_ibfk_1` FOREIGN KEY (`organization_id`) "
+    "  CONSTRAINT `buildings_ibfk_1` FOREIGN KEY (`organization_id`)"
     "    REFERENCES `organization` (`organization_id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
-TABLES['devices'] = (
-    "CREATE TABLE `devices` ("
+TABLES['device'] = (
+    "CREATE TABLE `device` ("
     "  `device_id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `device_name` varchar(50) NULL COMMENT 'Device Reference',"
     "  `building_id` int(11) NOT NULL,"
     "  `device_type` varchar(50) NOT NULL COMMENT 'Device Features',"
     "  `device_role` varchar(50) NOT NULL COMMENT 'Device Purpose',"
-    "  `active` BOOLEAN NOT NULL DEFAULT '1' COMMENT 'Active?',"
-    "  `created_at` date NOT NULL COMMENT 'Date created',"
-    "  `updated_at` date NOT NULL COMMENT 'Date last updated',"
+    "  `active` tinyint NOT NULL DEFAULT '1' COMMENT 'Active?',"
+    "  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'Date created',"
+    "  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() COMMENT 'Date last updated',"
     "  PRIMARY KEY (`device_id`), UNIQUE KEY `device_name` (`device_name`),"
     "  CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`building_id`) "
     "    REFERENCES `buildings` (`building_id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
-TABLES['device_log'] = (
-    "CREATE TABLE `device_log` ("
-    "  `device_id` int(11) NOT NULL,"
-    "  `device_name` varchar(50) NULL COMMENT 'Device Reference',"
+TABLES['data_log'] = (
+    "CREATE TABLE `data_log` ("
+    "  `device_name` varchar(50) NOT NULL COMMENT 'Device Reference',"
     "  `data_type` varchar(50) NOT NULL,"
-    "  `data_payload` varchar(50) NULL,"
-    "  `created_at` date NOT NULL COMMENT 'Date created',"
-    "  PRIMARY KEY (`created_at`)"
+    "  `data_payload` varchar(250) NULL,"
+    "  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'Date created',"
+    "  PRIMARY KEY (`data_type`, `created_at`)"
     ") ENGINE=InnoDB")
 
 
 # Initial DB connection
 connection = mysql.connector.connect(user=USER_NAME,
-                              password=PASSWORD)
+                              password=PASSWORD,
+                              host=DB_HOST)
 
 cursor = connection.cursor()
 
-# If the DB does not exist, and the user has permissions, go ahead and create it
 def create_database(cursor):
     try:
         cursor.execute(
@@ -76,6 +77,8 @@ def create_database(cursor):
         print("Failed creating database: {}".format(err))
         exit(1)
 
+
+##########################################################
 # Make tentative connection to the DB
 # Create DB if necessary
 try:
@@ -83,6 +86,7 @@ try:
 except mysql.connector.Error as err:
     print("Database {} does not exists.".format(DB_NAME))
     if err.errno == errorcode.ER_BAD_DB_ERROR:
+        # If the DB does not exist, and the user has permissions, go ahead and create it
         create_database(cursor)
         print("Database {} created successfully.".format(DB_NAME))
         connection.database = DB_NAME
@@ -90,6 +94,8 @@ except mysql.connector.Error as err:
         print(err)
         exit(1)
 
+
+##########################################################
 # At this point the DB should be in place and we are ready to start adding tables
 for table_name in TABLES:
     table_description = TABLES[table_name]
